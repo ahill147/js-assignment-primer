9105f8fdc026d0788c6780fa5468f6769b9f02dc
Drew Hill <130328421+ahill147@users.noreply.github.com>
2023-06-06 17:40:53 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+export function calculateTotal(items, tax) {
+  let total = 0;
+  
+  for (let item of items) {
+    const { price, taxable } = item;
+    if (taxable) {
+      total += price + price * Math.abs(tax);
+    } else {
+      total += price;
+    }
+  }
+  return total;
+}
